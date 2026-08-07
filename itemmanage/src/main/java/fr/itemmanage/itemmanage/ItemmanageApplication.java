0dff136e55e14654fc565de1baed4f9372ef20fd
eliess <eloykreir85@gmail.com>
2026-08-07 11:38:59 +0200

    chore: structure initiale backend + frontend

---
+++ itemmanage/src/main/java/fr/itemmanage/itemmanage/ItemmanageApplication.java
@@ -0,0 +1,13 @@
+package fr.itemmanage.itemmanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemmanageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItemmanageApplication.class, args);
+	}
+
+}
---
+++ itemmanage/src/test/java/fr/itemmanage/itemmanage/ItemmanageApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.itemmanage.itemmanage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemmanageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
